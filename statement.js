0c112d870da356d3f0f10025c3fb24a048a263d1
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2018-12-23 18:47:48 -0200

    Create render for plain text function

--- statement.js
+++ statement.js
@@ -1,6 +1,23 @@
 function statement (invoice, plays) {
     
-    function amountFor(aPerformance) {
+    return renderPlainText(invoice, plays);
+
+}
+
+function renderPlainText(invoice, plays) {
+    
+    let result = `Statement for ${invoice.customer}\n`
+
+    for(let perf of invoice.performances) {
+        // print line for this order
+        result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`
+    return result;   
+
+        function amountFor(aPerformance) {
         let result = 0;
 
         switch (playFor(aPerformance).type) {
@@ -58,16 +75,6 @@ function statement (invoice, plays) {
         return result;
     }
 
-    let result = `Statement for ${invoice.customer}\n`
-
-    for(let perf of invoice.performances) {
-        // print line for this order
-        result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`
-    return result;
 }
 
 module.exports = {statement: statement};
