4b1c199b5737e870cbc4de480b6d42dcc7d932f6
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2018-12-24 11:40:16 -0200

    Add html version of statement

--- test/test.js
+++ test/test.js
@@ -12,8 +12,20 @@ var expectedInvoice = `Statement for BigCo
 Amount owed is $1,730.00
 You earned 75 credits\n`;
 
+var expectedHtmlInvoice = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>    <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+    <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+    <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>75</em> credits</p>\n`
+
 describe('Statement', function() {
     it('Invoice statement for BigCo', function() {
       assert.equal(st.statement(invoice[0], plays), expectedInvoice);
+  });
+    it('HTML Invoice statement for BigCo', function() {
+      assert.equal(st.htmlStatement(invoice[0], plays), expectedHtmlInvoice);
   });
 });
